af7c33f6df4cbaa5623aac104c63350766d6bc74
GavrilS <gavr1lll@abv.bg>
2024-08-12 23:02:26 +0300

    The web server sends a greeting when opening and if you type your name it will greet you personally

--- custom_python_webserver/build_custom_server_v2.py
+++ custom_python_webserver/build_custom_server_v2.py
@@ -17,7 +17,7 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(bytes(file_to_open), 'utf-8')
+            self.wfile.write(bytes(file_to_open, 'utf-8'))
         except:
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
